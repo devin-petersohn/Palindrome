abbf828616e2f200bfd08261b5940f201b498034
Devin Petersohn <dpvx8@mail.missouri.edu>
2016-01-08 05:19:15 -0600

    Created batch option for script

--- master_run_script.sh
+++ master_run_script.sh
@@ -58,3 +58,33 @@ done
 exit
 
 fi
+if test "$#" -eq 7; then
+
+filepath=$1
+minimum=$2
+master=$3
+executor_num=$4
+executor_mem=$5
+executor_cores=$6
+master_mem=$7
+
+(cd cleanUpData && ./cleanUpFirst $filepath)
+	if test "$?" -ne 0; then
+		echo "Error encountered.
+"
+		exit
+	fi
+(cd cleanUpData && spark-submit target/scala-2.10/cleanupdata_2.10-0.1.jar ../intermediate_data/cleanOutput_stage1.txt $minimum)
+rm intermediate_data/cleanOutput_stage1.txt
+
+for f in `ls intermediate_data`
+do
+	(cd PalindromeFinder && spark-submit --master $master --driver-memory $master_mem --num-executors $executor_num --executor-cores executor-cores --executor-memory executor_mem --class PalindromeFinder target/scala-2.10/palindromefinder_2.10-0.1.jar ../intermediate_data/$f $minimum)
+done
+
+
+exit
+fi
+
+echo "usage: master_run_script.sh <filepath> <minimum> <master> <executor_num> <executor_mem> <executor_cores> <master_mem>
+"
