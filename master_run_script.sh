455c726672047da995ec1a75ac53d8651f13e0b9
Devin Petersohn <dpvx8@mail.missouri.edu>
2016-01-12 14:02:09 -0600

    More bug fixes

--- master_run_script.sh
+++ master_run_script.sh
@@ -1,4 +1,4 @@
-echo "
+echo -e "
 
 ==========================    PalindromeFinder    ==========================
 ==========================        iDas Lab        ==========================
@@ -18,41 +18,40 @@ mkdir intermediate_data
 (cd PalindromeFinder && sbt package)
 
 if test "$#" -eq 0; then
-echo "
-Please give the path to the file you would like to process.
-"
+echo -e "
+Please give the path to the file you would like to process.	"
 read filepath
 (cd cleanUpData && ./cleanUpFirst $filepath)
 	if test "$?" -ne 0; then
-		echo "Error encountered.
+		echo -e "Error encountered.
 "
 		exit
 	fi
-echo "What is the minimum length palindrome you would like to identify?  "
+echo -e "What is the minimum length palindrome you would like to identify?  "
 read minimum
-echo "Please wait while the file is parsed..."
+echo -e "Please wait while the file is parsed..."
 (cd cleanUpData && spark-submit target/scala-2.10/cleanupdata_2.10-0.1.jar ../intermediate_data/cleanOutput_stage1.txt $minimum)
 rm intermediate_data/cleanOutput_stage1.txt
-echo "Please enter the following Spark configuration parameters: 
-Master Node? "
+echo -e "Please enter the following Spark configuration parameters: 
+Master Node? \c"
 read master 
-echo "Number of executors? "
+echo -e "Number of executors? \c"
 read executor_num
-echo "Memory per executor? "
+echo -e "Memory per executor? \c"
 read executor_mem
-echo "Number of cores per executor? "
+echo -e "Number of cores per executor? \c"
 read executor_cores
-echo "Master memory? "
+echo -e "Master memory? \c"
 read master_mem
 
-echo "
+echo -e "
 Thank you. Starting to process. Please wait...
 
 "
 
 for f in `ls intermediate_data`
 do
-	(cd PalindromeFinder && spark-submit --master $master --driver-memory $master_mem --num-executors $executor_num --executor-cores $executor-cores --executor-memory $executor_mem --class PalindromeFinder target/scala-2.10/palindromefinder_2.10-0.1.jar ../intermediate_data/$f $minimum)
+	(cd PalindromeFinder && spark-submit --master $master --driver-memory $master_mem --num-executors $executor_num --executor-cores $executor_cores --executor-memory $executor_mem --class PalindromeFinder target/scala-2.10/palindromefinder_2.10-0.1.jar ../intermediate_data/$f $minimum)
 done
 
 exit
@@ -70,7 +69,7 @@ master_mem=$7
 
 (cd cleanUpData && ./cleanUpFirst $filepath)
 	if test "$?" -ne 0; then
-		echo "Error encountered.
+		echo -e "Error encountered.
 "
 		exit
 	fi
@@ -79,12 +78,11 @@ rm intermediate_data/cleanOutput_stage1.txt
 
 for f in `ls intermediate_data`
 do
-	(cd PalindromeFinder && spark-submit --master $master --driver-memory $master_mem --num-executors $executor_num --executor-cores $executor-cores --executor-memory $executor_mem --class PalindromeFinder target/scala-2.10/palindromefinder_2.10-0.1.jar ../intermediate_data/$f $minimum)
+	(cd PalindromeFinder && spark-submit --master $master --driver-memory $master_mem --num-executors $executor_num --executor-cores $executor_cores --executor-memory $executor_mem --class PalindromeFinder target/scala-2.10/palindromefinder_2.10-0.1.jar ../intermediate_data/$f $minimum)
 done
 
 
 exit
 fi
 
-echo "usage: master_run_script.sh <filepath> <minimum> <master> <executor_num> <executor_mem> <executor_cores> <master_mem>
-"
+echo -e "usage: master_run_script.sh <filepath> <minimum> <master> <executor_num> <executor_mem> <executor_cores> <master_mem>"
