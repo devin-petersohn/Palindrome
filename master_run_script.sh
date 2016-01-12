f77beb55321782c08121db076a6c431ba5187f5f
Devin Petersohn <dpvx8@mail.missouri.edu>
2016-01-12 14:35:47 -0600

    Files are now loaded from local FS instead of HDFS

--- master_run_script.sh
+++ master_run_script.sh
@@ -7,8 +7,8 @@ echo -e "
 
 Please note: This script only supports one FASTA file at a time
 
-Please wait while the scripts are compiled and prepared.
 
+Please wait while the scripts are compiled and prepared.
 "
 
 (cd cleanUpData && make all)
@@ -19,7 +19,7 @@ mkdir intermediate_data
 
 if test "$#" -eq 0; then
 echo -e "
-Please give the path to the file you would like to process.	"
+Please give the path to the file you would like to process:	\c"
 read filepath
 (cd cleanUpData && ./cleanUpFirst $filepath)
 	if test "$?" -ne 0; then
@@ -27,9 +27,10 @@ read filepath
 "
 		exit
 	fi
-echo -e "What is the minimum length palindrome you would like to identify?  "
+echo -e "What is the minimum length palindrome you would like to identify?  \c"
 read minimum
-echo -e "Please wait while the file is parsed..."
+echo -e "
+Please wait while the file is parsed and prepared..."
 (cd cleanUpData && spark-submit target/scala-2.10/cleanupdata_2.10-0.1.jar ../intermediate_data/cleanOutput_stage1.txt $minimum)
 rm intermediate_data/cleanOutput_stage1.txt
 echo -e "Please enter the following Spark configuration parameters: 
@@ -46,12 +47,11 @@ read master_mem
 
 echo -e "
 Thank you. Starting to process. Please wait...
-
 "
 
 for f in `ls intermediate_data`
 do
-	(cd PalindromeFinder && spark-submit --master $master --driver-memory $master_mem --num-executors $executor_num --executor-cores $executor_cores --executor-memory $executor_mem --class PalindromeFinder target/scala-2.10/palindromefinder_2.10-0.1.jar ../intermediate_data/$f $minimum)
+	(cd PalindromeFinder && spark-submit --master $master --driver-memory $master_mem --num-executors $executor_num --executor-cores $executor_cores --executor-memory $executor_mem --class PalindromeFinder target/scala-2.10/palindromefinder_2.10-0.1.jar file://`pwd`/../intermediate_data/$f $minimum)
 done
 
 exit
@@ -69,8 +69,7 @@ master_mem=$7
 
 (cd cleanUpData && ./cleanUpFirst $filepath)
 	if test "$?" -ne 0; then
-		echo -e "Error encountered.
-"
+		echo -e "Error encountered."
 		exit
 	fi
 (cd cleanUpData && spark-submit target/scala-2.10/cleanupdata_2.10-0.1.jar ../intermediate_data/cleanOutput_stage1.txt $minimum)
@@ -78,7 +77,7 @@ rm intermediate_data/cleanOutput_stage1.txt
 
 for f in `ls intermediate_data`
 do
-	(cd PalindromeFinder && spark-submit --master $master --driver-memory $master_mem --num-executors $executor_num --executor-cores $executor_cores --executor-memory $executor_mem --class PalindromeFinder target/scala-2.10/palindromefinder_2.10-0.1.jar ../intermediate_data/$f $minimum)
+	(cd PalindromeFinder && spark-submit --master $master --driver-memory $master_mem --num-executors $executor_num --executor-cores $executor_cores --executor-memory $executor_mem --class PalindromeFinder target/scala-2.10/palindromefinder_2.10-0.1.jar file://`pwd`/../intermediate_data/$f $minimum)
 done
 
 
