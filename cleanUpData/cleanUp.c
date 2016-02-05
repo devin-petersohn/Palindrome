cf557002fdd23133c9181346541a9a912e1e48bd
Devin Petersohn <dpvx8@mail.missouri.edu>
2016-02-05 13:38:04 -0600

    Working on making the pipeline faster. Currently not working

--- cleanUpData/cleanUp.c
+++ cleanUpData/cleanUp.c
@@ -4,6 +4,7 @@
 #include <libgen.h>
 
 #define BUFFER_LINES_IN 1000
+#define MAX_NUMBER_OF_LINES 100000
 
 int main(int argc, char **argv) {
 
@@ -18,7 +19,10 @@ int main(int argc, char **argv) {
 	strcat(output_filename, basename(argv[1]));
 	strcat(output_filename, ".clean");
 	FILE* cleanOutput = fopen(output_filename, "w");
-	free(output_filename);
+	FILE* list_of_files = fopen("../intermediate_data/list_of_files.clean");
+	fprintf(list_of_files, "%s\n", output_filename);
+	char buffer[256];
+	int file_count = 0;
 
 	if(!cleanOutput) {
 		fprintf(stderr, "%s\n", "File error. Unable to create.");
@@ -32,21 +36,36 @@ int main(int argc, char **argv) {
 		if(temporary_input_string[strlen(temporary_input_string)-1] == '\n'){
 			temporary_input_string[strlen(temporary_input_string)-1] = '\0';
 			if(temporary_input_string[0] == '>') {
+				if(current_size > MAX_NUMBER_OF_LINES) {
+					file_count++;
+					fclose(cleanOutput);
+					output_filename[strlen(output_filename) - 7] = '\0';
+					sprintf(buffer, "%d", file_count);
+					strcat(output_filename, buffer);
+					strcat(output_filename, ".clean");
+					cleanOutput = fopen(output_filename, "w");
+					fprintf(list_of_files, "%s\n", output_filename);
+					current_size = 0;
+					
+				}
 				if(current_size != 0) fprintf(cleanOutput, "\n");
 				fprintf(cleanOutput, "%s_%sBREAK_HERE_PALINDROME", basename(argv[1]), temporary_input_string);
-				current_size = 1;
-			} else {
 				current_size += 1;
+			} else {
 				fprintf(cleanOutput, "%s", temporary_input_string);
 			}
 		} else{
+			free(output_filename);
 			free(temporary_input_string);
 			fprintf(stderr, "%s%d\n", "Lines too long. Current line limit: ", BUFFER_LINES_IN);
 			return -1;
 		}
 	}
 
+	free(output_filename);
 	free(temporary_input_string);
+	fclose(cleanOutput);
+	fclose(list_of_files);
 	return 0;
 
 
